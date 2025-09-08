0d4059ad9bb1fdc0f545cf108a460dd3debc72ff
Andrei <astoyanow13@gmail.com>
2025-09-08 01:30:10 -0700

    Add rust stuff and more other stuff.

--- ds/src/ds/BreadthFirstSearch.java
+++ ds/src/ds/BreadthFirstSearch.java
@@ -1,7 +1,9 @@
 package ds;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import node.Node;
 
 public class BreadthFirstSearch {
 	
@@ -9,8 +11,21 @@ public class BreadthFirstSearch {
 		
 	}
 	
-	public List<?> search(ArrayList<?> list) {
-		return null;
+	public List<Node> search(Node start, String target) {
+		ArrayList<String> seen = new ArrayList<>();
+		List<Node> neighbors = start.getNeighbors();
+		int numNeighbors = neighbors.size();
+		if (numNeighbors == 0) {
+			return new ArrayList<Node>(Arrays.asList(start));
+		}
+		for (int i = 0; i < numNeighbors; i ++) {
+			if (numNeighbors = 0) {
+				
+			}
+		}
+		
 	}
 	
-}
+	public List<Node> subsearch(Node node, ArrayList<String> seen) {
+		List<Node> 
+	}
